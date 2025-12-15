61755fa9871a93a07243a90a9fb24842b0317cd3
Jaysmart <ogunbunmijoshua60@gmail.com>
2025-12-16 00:00:55 +0100

    Add LandingPage component and Navbar; update routing in App

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Route , Routes} from 'react-router-dom'
 import SignUp from './Pages/SignUp/SignUp.jsx'
 import SignIn from './Pages/SignIn/SignIn.jsx'
 import Dashboard from './Pages/Dashboard/Dashboard.jsx'
+import LandingPage from './Pages/LandingPage/LandingPage.jsx'
 
 
 function App() {
@@ -9,7 +10,8 @@ function App() {
   return (
     <>
       <Routes>
-         <Route path='/' element={<SignUp/>}/>
+        <Route path='/' element={<LandingPage/>}/>
+         <Route path='/signup' element={<SignUp/>}/>
         <Route path="/signin" element={<SignIn/>} />
         <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
--- Frontend/src/Pages/LandingPage/LandingPage.jsx
+++ Frontend/src/Pages/LandingPage/LandingPage.jsx
@@ -1,25 +1,11 @@
-// import React from 'react'
-// import './LandingPage.css'
+import React from 'react'
+import Navbar from '../Navbar/Navbar'
+const LandingPage = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
 
-// const LandingPage = () => {
-  
-  
-
-//   return (
-//     <div className='landing-page d-flex justify-content-center align-items-center bg-dark text-white vh-100 '>
-//       <div id='container'>
-//          <img id='loGo' className='img-fluid' src="https://res.cloudinary.com/dc4fx7sbe/image/upload/v1760653241/pill_j4t94m.png" alt="" />
-//          <h2 className='welcome-text'>Welcome To</h2>
-//          <h1 className='Med text-center'>MedReminder</h1>
-//          <p>Your personal medication reminder Site.</p>
-//           <button href="/SignUp" className='start mt-2 mb-2'>Get Started</button>
-//            <p className='text-center mt-4'>Already have an account?   <a href="https://pe-frontend-chi.vercel.app/signin">Sign In</a> </p>
-//           <footer>
-//             <p>© 2025 MedReminder. All rights reserved.</p>
-//           </footer>
-//       </div>
-//       </div>
-//   )
-// }
-
-// export default LandingPage
+export default LandingPage
